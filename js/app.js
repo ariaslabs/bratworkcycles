4195ddaef0b177e616da97ce386a9d386409595c
Christopher Arias <christopher.arias.jr@gmail.com>
2019-06-27 20:58:47 -0700

    version 6

--- js/app.js
+++ js/app.js
@@ -21,3 +21,40 @@ document.getElementById('seconds').innerHTML = ":" + secs;
 }
 //updates clock every second
 setInterval(clock,100);
+
+
+//submit info for contact page
+function submitInfo() {
+  var amountA = document.getElementById("numberinput").value;//Getting value from number input
+  var amountB = "$" + (amountA * 125);//doing the math for caculation the amount a sponser cost
+  .innerHTML = amountB;//attempting to display var amountB
+}
+document.getElementById("submitOne") .addEventListener("click", submitInfo, false); //added a event listener
+document.querySelector('.results').innerHTML = amountB;//another attempt to display
+
+//validator for contact us info on contact page
+var name = document.getElementById('name');
+var email = document.getElementById('email');
+var form = document.getElementById('textarea');
+var errorElement = document.getElementById('error');
+var thanksElements = document.getElementById('thanks');
+
+form.addEventListener('submit', (e)) {
+  let messages = []
+  if (name.value === '' || name.value == null) {
+    message.push('Name is required');
+  }
+  if (email.value === '' || email.value == null) {
+    message.push('email is required');
+  }
+  if (email.value <= 1) {
+    messages.push('email is required');
+  }
+  if (messages.length > 0) {
+    e.preventDefault();
+    errorElement.innerText = messages.join(', ');
+  }
+  else if () {
+    alert("thank You!");
+  }
+});
